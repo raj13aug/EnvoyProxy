5ca33a2aa4b59e78112f7ec1edd38e732024b094
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2026-03-20 21:01:29 +0530

    updated 3

--- Terraform/helm.tf
+++ Terraform/helm.tf
@@ -1,8 +1,18 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 resource "helm_release" "envoy_gateway" {
   name          = "envoy-gateway"
   repository    = "oci://docker.io/envoyproxy"
   chart         = "gateway-helm"
-  namespace     = "envoy"
+  namespace     = "envoy-gateway-system"
   values        = [file("${path.module}/configs/envoy-gateway-values.yaml")]
   version       = "v1.6.1"
   recreate_pods = true
