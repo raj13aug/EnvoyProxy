4c2e1cc2c9e559887edc55727631bdb1a20e8457
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2026-03-22 19:02:26 +0530

    g

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,13 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-provider "google-beta" {
-  project = var.project_id
-  region  = var.region
-}
-
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
@@ -15,5 +5,21 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
+  }
+}
+
+
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.17.0, < 3.0.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
   }
 }
