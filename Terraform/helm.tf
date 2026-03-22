9d9f35cb5802d3800162ce448477ad1e69d70d06
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2026-03-22 18:56:06 +0530

    Update helm.tf

--- Terraform/helm.tf
+++ Terraform/helm.tf
@@ -1,9 +1,9 @@
 
 resource "helm_release" "envoy_gateway" {
-  name          = "envoy-gateway"
-  repository    = "oci://docker.io/envoyproxy"
-  chart         = "gateway-helm"
-  namespace     = "envoy-gateway-system"
-  version       = "latest"
-  recreate_pods = true
+  name             = "envoy-gateway"
+  repository       = "oci://docker.io/envoyproxy"
+  chart            = "gateway-helm"
+  namespace        = "envoy-gateway-system"
+  version          = "v1.7.1"
+  create_namespace = true
 }
