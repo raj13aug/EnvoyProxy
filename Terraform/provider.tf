d611988b233d15dca1c2b9338c4cc22ab769f5c7
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2026-03-22 19:10:08 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,25 +1,6 @@
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
 }
 
-
-
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.17.0, < 3.0.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
-    }
-  }
-}
